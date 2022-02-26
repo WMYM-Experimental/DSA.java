c72620696b7134b8845fe313b90dd5f154f855a6
WashingtonYandun <washington.yandun06@gmail.com>
2022-02-26 15:44:39 -0500

    start making algorithms

---
+++ src/com/dsa/algo/leet_code/TwoSum.java
@@ -0,0 +1,31 @@
+// Two Sum
+
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+*/
+package com.dsa.algo.leet_code;
+
+import java.util.Arrays;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(compute(new int[]{1, 2, 3, 4, 5, 9, 10, 20, 19}, 20)));
+    }
+
+    public static int[] compute(int[] arr, int sum){
+        int[] sol = new int[2];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i]+arr[j] == sum){
+                    sol[0] = i;
+                    sol[1] = j;
+                    return sol;
+                }
+            }
+        }
+        return sol;
+
+    }
+}
---
+++ src/com/dsa/algo/project_euler/ID_001.java
@@ -0,0 +1,24 @@
+// Multiples of 3 or 5
+/*
+Problem 1
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+*/
+package com.dsa.algo.project_euler;
+
+public class ID_001 {
+    public static void main(String[] args) {
+        System.out.println(MultiplesOf3Or5(1000)); // 233168
+    }
+
+    public static int MultiplesOf3Or5(int target){
+        int counter = 0;
+        for (int i = 0; i < target ; i++) {
+            if (i % 3 == 0 || i % 5 == 0){
+                counter += i;
+            }
+        }
+        return counter;
+    }
+}
