4a9398b0bacdd9b792a3a2f6ca7ea66bfa70839a
WashingtonYandun <washington.yandun06@gmail.com>
2022-05-10 12:59:49 -0500

    finished basic implementation of the CircularLinkedList class

--- src/com/dsa/Main.java
+++ src/com/dsa/Main.java
@@ -7,9 +7,11 @@ public class Main {
     public static void main(String[] args) {
         CircularLinkedList cll = new CircularLinkedList();
         System.out.println(cll.toString());
-        for (int i = 0; i < 10; i++) {
+        for (int i = 100; i < 1000; i+=100) {
             cll.addEnd(i);
         }
         System.out.println(cll.toString());
+        System.out.println(cll.remove(200));
+        System.out.println(cll.toNodesString());
     }
 }
--- src/com/dsa/data_structs/cll/CircularLinkedList.java
+++ src/com/dsa/data_structs/cll/CircularLinkedList.java
@@ -28,6 +28,31 @@ public class CircularLinkedList {
         this.tail = aux;
     }
 
+    public boolean remove(int data){
+        CllNode aux = this.head;
+        if(this.head == null){
+            return false;
+        }
+
+        // this is case when the data we want to remove is the head
+        if(this.head.data == data){
+            this.head = this.head.next;
+            this.tail.next = this.head;
+            this.tail = this.head;
+            return true;
+        }
+
+        do {
+            if(aux.next.data == data){
+                aux.next = aux.next.next;
+                return true;
+            }
+            aux = aux.next;
+        }while(aux != this.head);
+
+        return false;
+    }
+
     @Override
     public String toString() {
         StringBuilder result = new StringBuilder("[ ");
@@ -42,6 +67,27 @@ public class CircularLinkedList {
                 result.append(aux.data);
             }else {
                 result.append(aux.data).append(", ");
+            }
+            aux = aux.next;
+        }while (aux != this.head);
+
+        result.append(" ]");
+        return result.toString();
+    }
+
+    public String toNodesString() {
+        StringBuilder result = new StringBuilder("[ ");
+        CllNode aux = this.head;
+
+        if(this.head == null){
+            return "[]";
+        }
+
+        do {
+            if(aux.next == this.head) {
+                result.append(aux.toString());
+            }else {
+                result.append(aux.toString()).append(", ");
             }
             aux = aux.next;
         }while (aux != this.head);
