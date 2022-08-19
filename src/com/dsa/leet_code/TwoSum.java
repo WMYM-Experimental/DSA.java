27440b641308a260b56c818bd51e670d82c024c9
WMYM <washington.yandun@udla.edu.ec>
2022-08-18 19:17:36 -0500

    move  folder because leetcode dir will be more important in the future

--- src/com/dsa/leet_code/TwoSum.java
+++ src/com/dsa/leet_code/TwoSum.java
@@ -5,7 +5,7 @@ Given an array of integers nums and an integer target, return indices of the two
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 You can return the answer in any order.
 */
-package com.dsa.algo.leet_code;
+package com.dsa.leet_code;
 
 import java.util.Arrays;
 
